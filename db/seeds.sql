531f66151b3ad7e77e4533cb6669f0b0439b04ad
Kristina Alabastro <kristinaa7@live.com>
2023-02-19 20:18:38 -0800

    added all prompt questions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,29 @@
-INSERT INTO department (id, name)
-VALUES  (2, 'Engineering'), 
-        (3, 'Finance'), 
-        (4, 'Legal'), 
-        (1, 'Sales'),
+INSERT INTO department (name)
+VALUES  ( 'Sales'),
+        ( 'Engineering'), 
+        ( 'Finance'), 
+        ( 'Legal');
+
+-- autoincrement in schema.sql makes you not need to rewrite your Id
+-- semi colons on the last value
+
+INSERT INTO role (title, department_id, salary) 
+VALUES  ('Sales Lead', 1, 100000),
+        ('Salesperson', 1, 80000),
+        ('Lead Engineer', 2, 150000),
+        ('Software Engineer', 2, 120000),
+        ( 'Account Manager', 3, 160000),
+        ('Accountant', 3, 125000),
+        ('Legal Team Lead', 4, 250000),
+        ('Lawyer', 4, 190000);
 
 
-INSERT INTO role (id, title, department_id, salary) 
-VALUES  (1, 'Sales Lead', 1, 100000),
-        (2,'Salesperson', 1, 80000),
-        (3,'Lead Engineer', 2, 150000),
-        (4,'Software Engineer', 2, 120000),
-        (5, 'Account Manager', 3, 160000),
-        (6,'Accountant', 3, 125000),
-        (7,'Legal Team Lead', 4, 250000),
-        (8,'Lawyer', 4, 190000),
-
-
-INSERT INTO employee (id, first_name, last_name, role_id, department_id, salary, manager_id)
-VALUES  (1, 'John', 'Doe', 1, 1, 100000, NULL),
-        (2, 'Mike', 'Chan', 2, 1, 80000, 1), 
-        (3, 'Ashley', 'Rodriguez', 3, 2, 150000, NULL),
-        (4, 'Kevin', 'Tupik', 4, 2, 120000, 3),
-        (5, 'Kunal', 'Singh', 5, 3, 160000 NULL),
-        (6, 'Malia', 'Brown', 6, 3, 125000, 5),
-        (7, 'Sarah', 'Lourd', 7, 4, 250000, NULL),
-        (8, 'Tom', 'Allen', 8, 4, 190000, 7),
+INSERT INTO employee (first_name, last_name, role_id, department_id, salary, manager_id)
+VALUES  ('John', 'Doe', 1, 1, 100000, NULL),
+        ('Mike', 'Chan', 2, 1, 80000, 1), 
+        ('Ashley', 'Rodriguez', 3, 2, 150000, NULL),
+        ('Kevin', 'Tupik', 4, 2, 120000, 3),
+        ('Kunal', 'Singh', 5, 3, 160000 NULL),
+        ('Malia', 'Brown', 6, 3, 125000, 5),
+        ('Sarah', 'Lourd', 7, 4, 250000, NULL),
+        ('Tom', 'Allen', 8, 4, 190000, 7);
--- index.js
+++ index.js
@@ -2,15 +2,122 @@ const inquirer = require('inquirer')
 
 const questions = () => {
     console.log(`Employee Tracker`);
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+            name: 'start'
+        },
+    ])
+        .then((answer) => {
+            switch (answer.start) {
+                case 'View All Employees':
+                    allEmployees();
+                    break;
+                case 'Add Employees':
+                    addEmployees();
+                    break;
+                case 'Update Employee Role':
+                    employeeRole();
+                    break;
+                case 'View All Roles':
+                    viewAll();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    allDepartments();
+                    break;
+            }
+
+        })
+};
+
+//returning the table
+const allEmployees = () => {
+    // employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+};
+
+//requires prompts
+const addEmployees = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: "What is the employee's first name?",
+            name: 'firstName', 
+        },
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'lastName', 
+        },
+        {
+            type: 'list',
+            message: "What is the employee's role?",
+            choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead','Lawyer', 'Customer Service'],
+            name: 'role'
+        },
+        {
+            type: 'list',
+            message: "Who is the employee's manager?",
+            choices: ['John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malia Brown'],
+            name: 'manager'
+        },
+    ])
+    .then
+console.log('Added `{$firstName}` `{$lastName}` to the database');
+};
+
+//requires prompts
+const employeeRole = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: "Which employee's role do you want to update?",
+            name: 'updateName', 
+        },
+    {
+        type: 'list',
+        message: "Which role do you want to assign to the selected employee?",
+        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead','Lawyer', 'Customer Service'],
+        name: 'updateRole'
+    },
+    ])
+    .then
+    console.log("Updated employee's role");
+};
+
+//returning the table
+const viewAll = () => {
+};
+
+//requires prompts
+const addRole = () => {
     inquirer.prompt ([
     {
-    type: 'list',
-    message: 'What would you like to do?',
-    choices: ['View All Employees','Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-    default: 'View All Employees',
-    name: 'actionKeys'
+        type: 'input',
+        message: 'What is the name of the role?',
+        name: 'name', 
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'salary', 
+    },
+    {
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service'],
+        name: 'department'
     },
 ])
-.then(answer => {
-    console.log('Answer',answer.actionKeys);
-})};
+.then
+console.log('Added Customer Service to the database');
+};
+
+//returning the table
+const allDepartments = () => {
+};
